6fcf569bcb69983c1b357b3ef4af0584497fb223
Gustavo Gino Scotton <37678751+gustavogino@users.noreply.github.com>
2018-08-24 02:40:05 -0300

    Add files via upload

--- 1-primeiros-passos/main-maça.py
+++ 1-primeiros-passos/main-maça.py
@@ -10,8 +10,8 @@ canais=img.shape[2]
 for i in range(altura):
 	for j in range(largura):
 		if (img[i,j,2] > 110 and img[i,j,1]<140 and img[i,j,0]<140):
-			img[i,j,2]= 15
-			img[i,j,0]=255
+			img[i,j,2]= 0
+			img[i,j,0]=float(img[i,j,0])+float(110)
 
 cv2.imwrite("maca1.ppm", img)
 print("Imagem gerada com sucesso!")
